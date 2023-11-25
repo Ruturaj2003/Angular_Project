aa7fd2c91a4d19b0090797364320cba90068f445
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-11-25 10:27:56 +0530

    Assignment1
    Working with Directives like NgStyle NgClass and Structural Directives like *ngIf *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular_Project';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
