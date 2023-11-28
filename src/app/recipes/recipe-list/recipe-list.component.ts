17b047007238d227f00c8fcf6c2b42b55fa736a5
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-11-28 21:15:44 +0530

    Recipe Data Will Be Displayed By recipe-item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,5 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [new Recipe('Dota', 'dotex Res', 'this is dummy')];
+  samp =
+    'https://imgs.search.brave.com/484rbVGrzOSKVTk2N8LqU-XPvJD8oTHW6PN1QVQidDs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NDAxODk1NDkzMzYt/ZTZlOTljMzY3OWZl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4bGVI/QnNiM0psTFdabFpX/UjhOWHg4ZkdWdWZE/QjhmSHg4ZkE9PQ.jpeg';
+  recipes: Recipe[] = [
+    new Recipe('Dota', 'dotex Res', this.samp),
+    new Recipe('Dota', 'dotex Res', 'this is dummy'),
+  ];
 }
